5e5685c96d3f63fbfcbfe39e7e7093a78e0db802
Administrator_xfl <Administrator_xfl@aliyun.com>
2019-03-18 14:08:02 +0800

    maven项目整合idea测试

---
+++ src/main/java/com/xfl/bean/Car.java
@@ -0,0 +1,24 @@
+package com.xfl.bean;
+
+public class Car {
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    private String speed;
+    private String height;
+
+}
